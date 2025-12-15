c233f9689ed599c8adfa536ddaf9f1822b61e529
Vlad Kolhanov <vladi.kolganov@gmail.com>
2025-12-15 17:27:25 +0200

    🏗️ build(api): disallow direct use of process.env

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -1,3 +1,20 @@
 import baseConfig from '../../eslint.config.mjs'
 
-export default [...baseConfig]
+export default [
+  ...baseConfig,
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    ignores: ['**/env.module.ts'],
+    rules: {
+      'no-restricted-properties': [
+        'error',
+        {
+          object: 'process',
+          property: 'env',
+          message:
+            "Use `import { EnvModule } from '@/core/env/env.module'` instead to ensure validated types.",
+        },
+      ],
+    },
+  },
+]
